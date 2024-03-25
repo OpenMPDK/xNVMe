bce774d41c373b2c9706c08362bb9d09f77a2b69
Nadja Brix Koch <n.koch@samsung.com>
2024-03-25 12:33:00 +0100

    feat(tools/lblk): Add support for compare
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- cijoe/tests/tools/test_lblk.py
+++ cijoe/tests/tools/test_lblk.py
@@ -37,6 +37,15 @@ def test_write(cijoe, device, be_opts, cli_args):
     assert not err
 
 
+@xnvme_parametrize(labels=["dev"], opts=["be", "admin"])
+def test_compare(cijoe, device, be_opts, cli_args):
+    src = "/tmp/file.bin"
+
+    err, _ = cijoe.run(f"lblk read {cli_args} --slba 0x0 --nlb 0 --data-output {src}")
+    err, _ = cijoe.run(f"lblk compare {cli_args} --slba 0x0 --nlb 0 --data-input {src}")
+    assert not err
+
+
 @xnvme_parametrize(labels=["write_uncor"], opts=["be", "admin"])
 def test_write_uncor(cijoe, device, be_opts, cli_args):
     err, _ = cijoe.run(f"lblk write-uncor {cli_args} --slba 0x0 --nlb 0")
